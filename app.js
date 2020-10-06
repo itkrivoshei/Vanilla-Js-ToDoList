e2ce5d7f5417f3e628fd926c64dce457d6f5ab4c
Gitshey <krivoshey.nikita1998@gmail.com>
2020-10-06 13:34:50 +0300

    Add app.js and start vanilla codding :D

---
+++ app.js
@@ -0,0 +1,7 @@
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+document.addEventListener("DOMContentLoaded", getTodos);
+todoButton.addEventListener("click", addTodo);
+todoList.addEventListener("click", deleteTodo);
